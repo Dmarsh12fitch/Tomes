63a0f54698987d4e8848fcd9f667d6de4ebb938f
cgulick77 <cgulick@student.fitchburgstate.edu>
2021-06-20 14:48:25 -0400

    EarthPlatform
    Platform moves from one pos to another

---
+++ Assets/Scripts/EarthPlatform.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EarthPlatform : MonoBehaviour
+{
+    public GameObject platform;
+    public GameObject OnPos;
+    public GameObject OffPos;
+
+    public bool Activated;
+
+    private float speed = 5.0f;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        // Platforms speed 
+        float platformSpeed = speed * Time.deltaTime;
+        // Toggles the platform from On/Off 
+        if (Activated == true){
+            platform.transform.position = Vector3.MoveTowards(platform.transform.position, OnPos.transform.position, platformSpeed );
+        } else {
+            platform.transform.position = Vector3.MoveTowards(platform.transform.position, OffPos.transform.position, platformSpeed );
+        }
+    }
+}
