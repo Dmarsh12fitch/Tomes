e5373f8d0468fcee40e0c9b130e8015f3a126704
Nicholas <nalesse@student.fitchburgstate.edu>
2021-06-19 21:54:08 -0400

    Auto stash before merge of "mainthing" and "origin/mainthing"

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -10,10 +10,32 @@ public class CameraController : MonoBehaviour
 
     [SerializeField] private Vector3 offset;
     
+    // controls the sensitivity of the camera rotation
+    [SerializeField] private float sensitivity;
+    private Vector2 rotation;
+
 
     // Update is called once per frame
     void Update()
     {
         transform.position = player.transform.position + offset;
+        RotateCamera();
+    }
+
+    private void RotateCamera()
+    {
+        // Allows for rotating the camera on the y axis when you hold down left mouse 
+        if (Input.GetKey(KeyCode.Mouse0))
+        {
+            // Gets the mouse y axis input
+            Vector2 input = new Vector2(0, Input.GetAxis("Mouse Y"));
+            // sets the speed of the rotation with sensitivity. And also sets the vector
+            rotation += input * (sensitivity * Time.deltaTime);
+            rotation.x = 45;
+            // converts the rotation into a Euler Angle
+            transform.localEulerAngles = new Vector3(rotation.y, rotation.x, 0);
+        }
+        
+
     }
 }
