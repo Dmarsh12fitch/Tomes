4c40d339e42797ba7b5ccddc8d7a71eeaf85f5b2
Nicholas <nalesse@student.fitchburgstate.edu>
2021-06-28 16:42:29 -0400

    Added Camera Switching
    Created a CameraManager Script that switches between two cameras. The cameraManager needs to be attached to an empty Game Object in the scene

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+   [SerializeField] private Camera camera1;
+   [SerializeField] private Camera camera2;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camera1.enabled = true;
+        camera2.enabled = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            // Inverts the enabled property on each camera. e.g if camera1 is enabled it disables it and vice versa 
+            camera1.enabled = !camera1.enabled;
+            camera2.enabled = !camera2.enabled;
+        }
+    }
+}
