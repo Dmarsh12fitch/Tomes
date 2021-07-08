e1cedb2fe7ca3686a64d0f102ddccd7760e6800c
cgulick77 <cgulick@student.fitchburgstate.edu>
2021-07-07 21:44:35 -0400

    Traps
    Axe trap prototype

---
+++ Assets/Scripts/TrapAxe.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapAxe : MonoBehaviour
+{
+    public bool axeToggle;
+
+    public GameObject Axe;
+
+    private float speed = 100.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+       if (axeToggle == true) {
+           Axe.transform.Rotate(Vector3.left * speed * Time.deltaTime);
+       }
+    }
+
+  
+}
