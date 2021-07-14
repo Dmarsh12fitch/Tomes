493183d19d711a2e5a3803d68811b13e4a774784
Nicholas <nalesse@student.fitchburgstate.edu>
2021-07-14 19:12:30 -0400

    Made Basic Wind Puzzle
    Made a basic wind Puzzle where the player needs to move various objects using the wind spell onto the matching sigil

---
+++ Assets/Scripts/Puzzles/WindPuzzle/Sigil.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sigil : MonoBehaviour
+{
+    // Lets you chose the Sigil type in the inspector 
+    public enum SigilType
+    {
+        Wind,
+        Fire,
+        Earth
+    }
+
+    public SigilType sigilType;
+
+    public bool activated = false;
+
+
+    private void OnTriggerEnter(Collider other)
+    {
+        // if the other object's tag matches the sigil type then the sigil gets activated
+        if (other.gameObject.CompareTag(sigilType.ToString()))
+        {
+            activated = true;
+        }
+    }
+    //Todo find a way to deactivate sigial if object leaves trigger
+}
---
+++ Assets/Scripts/Puzzles/WindPuzzle/WindPuzzle.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+namespace Puzzles.WindPuzzle
+{
+    public class WindPuzzle : MonoBehaviour
+    {
+        // Summary of the puzzle: the player needs to stop the axe trap by taking each object and move it onto the sigil
+        // that has a matching element using the wind spell. 
+        public TrapAxe axeTrap;
+
+       [SerializeField] private List<Sigil> sigilList = new List<Sigil>();
+        // Start is called before the first frame update
+        void Start()
+        {
+            axeTrap = GameObject.FindGameObjectWithTag("Axe Trap").GetComponent<TrapAxe>();
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // checks if every sigil is activated
+            bool allActivated = true;
+            foreach (Sigil sigil in sigilList)
+            {
+                if (!sigil.activated)
+                {
+                    allActivated = false;
+                    break;
+                }
+            }
+            // if all sigials are activated then the axe trap is stopped
+            axeTrap.axeToggle = !allActivated;
+        }
+    }
+}
