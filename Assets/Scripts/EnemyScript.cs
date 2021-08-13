0a0453432cdc769c220353d1c19fa59ef5a81159
AlexG <agemmell@student.fitchburgstate.edu>
2021-08-13 11:50:01 -0400

    Enemy Movement
    The enemy moves to the player at a certain distance.

--- Assets/Scripts/EnemyScript.cs
+++ Assets/Scripts/EnemyScript.cs
@@ -4,6 +4,32 @@ using UnityEngine;
 
 public class EnemyScript : MonoBehaviour{
 
+    private GameObject player;
+
+    //Stores the distance the player needs to be within
+    private float triggerDistance = 5.0f;
+
+    private float enemySpeed = 5.0f;
+
+
+    private void Start(){
+
+        player = GameObject.Find("Player");
+
+    }
+
+    void Update(){
+
+        transform.LookAt(player.transform);
+
+        //If the distance between the enemies position and the players position is within range
+        if (Vector3.Distance(transform.position, player.transform.position) <= triggerDistance){
+
+            transform.position += transform.forward * enemySpeed * Time.deltaTime;
+
+        }
+    }
+
     private void OnCollisionEnter(Collision collision){
 
         if(collision.gameObject.tag == "Player"){
